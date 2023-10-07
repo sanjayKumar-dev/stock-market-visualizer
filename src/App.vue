aa23f093353f6e7038d2323eef2cd4edfd7ed1db
Sanjay Kumar <sanjay.11801603@gmail.com>
2023-10-07 09:49:58 +0530

    Added Scss and bootstrap 5 to project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <h1>Welcome To Stock Market Visualizer</h1>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div id="red1">
+    <h2>Red 1</h2>
+  </div>
+
+  <div id="red2">
+    <h2>Red 2</h2>
+  </div>
+
+  <div id="red3">
+    <h2>Red 3</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/variable.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +54,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  #red1 {
+   background-color: $red1;
+ } 
+ #red2 {
+   background-color: $red2;
+ }
+ #red3 {
+   background-color: $red5;
+ }
 </style>
