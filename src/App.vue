3d16b34c8fb0f53866f38edd048a2dbda183db0d
Sanjay Kumar <sanjay.11801603@gmail.com>
2023-10-07 16:54:17 +0530

    Added Header and Sidenav

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
       <Headers />
+      <Sidenav />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Headers from './components/Header.vue'
+import Headers from './components/GenricComponent/Header.vue'
+import Sidenav from './components/GenricComponent/Sidenav.vue';
 
 export default {
   name: 'App',
 
   components: {
-    Headers
+    Headers,
+    Sidenav
   },
 }
 </script>
---
+++ src/components/GenricComponent/Header.vue
@@ -0,0 +1,23 @@
+<template>
+        <v-app-bar color="warn" density="compact">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+
+            <v-app-bar-title>Stock Market Visualizer</v-app-bar-title>
+
+            <template v-slot:append>
+                <v-btn icon> <v-icon> mdi-heart </v-icon> </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+                <v-btn icon="mdi-dots-vertical"></v-btn>
+            </template>
+        </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
